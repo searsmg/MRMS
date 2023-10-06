6c43ca08c354bc3a43abec2d899cc5105d88ee94
Megan Sears <megan.sears@colostate.edu>
2023-10-06 16:32:42 -0600

    getting mrms 2023 scripts ready

--- scrape_mrms_multisensor.py
+++ scrape_mrms_multisensor.py
@@ -3,23 +3,23 @@ from urllib.request import HTTPError
 from datetime import datetime
 from datetime import timedelta
 import os
-###
 
-start = datetime(2021, 6, 1, 0, 0)
-end = datetime(2021, 7, 31, 23, 0)
+start = datetime(2023, 4, 30, 0, 0)
+end = datetime(2023, 10, 2, 0, 0)
 hour = timedelta(hours=1)
+#minute = timedelta(minutes=2)
 
 missing_dates = []
-fallback_to_radaronly = True #Enables a post-processing step that will go through the list of missing dates for gage-corrected
+fallback_to_radaronly = False #Enables a post-processing step that will go through the list of missing dates for gage-corrected
 ############################# and tries to go get the radar-only values if they exist.
 
-destination = "/Users/megansears/Documents/MRMS/QPE/2021"
+destination = "/Volumes/WCNR-Network/Research/Kampf/Private/SearsM/MRMS_2023_multisensor"
 
 date = start
 
 while date <= end:
-    url = "http://mtarchive.geol.iastate.edu/{:04d}/{:02d}/{:02d}/mrms/ncep/GaugeCorr_QPE_01H/GaugeCorr_QPE_01H_00.00_{:04d}{:02d}{:02d}-{:02d}0000.grib2.gz".format(
-        date.year, date.month, date.day, date.year, date.month, date.day, date.hour)
+    url = "http://mtarchive.geol.iastate.edu/{:04d}/{:02d}/{:02d}/mrms/ncep/MultiSensor_QPE_01H_Pass2/MultiSensor_QPE_01H_Pass2_00.00_{:04d}{:02d}{:02d}-{:02d}0000.grib2.gz".format(
+        date.year, date.month, date.day, date.year, date.month, date.day, date.hour, date.minute)
     filename = url.split("/")[-1]
     try:
         fetched_request = urllib.request.urlopen(url)
@@ -29,7 +29,7 @@ while date <= end:
         with open(destination + os.sep + filename, 'wb') as f:
             f.write(fetched_request.read())
     finally:
-        date += hour
+        date += minute
 
 if fallback_to_radaronly:
     radar_also_missing = []
